156fd19915bc2347bd8bebd7a4a008fbe6a9b1fa
dakotasanchez <dakota@dakotasanchez.com>
2015-09-01 00:46:24 -0700

    Market list item

--- app/src/main/java/com/sanchez/fmf/model/MarketListItemModel.java
+++ app/src/main/java/com/sanchez/fmf/model/MarketListItemModel.java
@@ -4,4 +4,26 @@ package com.sanchez.fmf.model;
  * Created by dakota on 8/31/15.
  */
 public class MarketListItemModel {
+
+    private String mId;
+    private String mName;
+    private double mDistanceFromQuery;
+
+    public MarketListItemModel(String id, String name, double distanceFromQuery) {
+        mId = id;
+        mName = name;
+        mDistanceFromQuery = distanceFromQuery;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public double getDistanceFromQuery() {
+        return mDistanceFromQuery;
+    }
 }
