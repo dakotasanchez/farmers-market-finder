0d7a7de2876b4830690a6f8d78e3dac8854fe993
dakotasanchez <dakota@dakotasanchez.com>
2015-09-01 00:48:41 -0700

    Create list and bind fake markets after 3s

--- app/src/main/java/com/sanchez/fmf/fragment/MarketListFragment.java
+++ app/src/main/java/com/sanchez/fmf/fragment/MarketListFragment.java
@@ -2,13 +2,32 @@ package com.sanchez.fmf.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sanchez.fmf.R;
+import com.sanchez.fmf.adapter.MarketAdapter;
+import com.sanchez.fmf.model.MarketListItemModel;
+import com.sanchez.fmf.util.MarketUtils;
+import com.sanchez.fmf.util.ViewUtils;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MarketListFragment extends Fragment {
+
+    @Bind(R.id.market_list)
+    RecyclerView mMarketList;
+    @Bind(R.id.fragment_default_suggestion)
+    View mSuggestion;
+
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public static MarketListFragment newInstance() {
         MarketListFragment fragment = new MarketListFragment();
@@ -34,6 +53,28 @@ public class MarketListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_market_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_market_list, container, false);
+        ButterKnife.bind(this, v);
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mMarketList.setLayoutManager(mLayoutManager);
+
+        ArrayList<MarketListItemModel> mMarkets = MarketUtils.getExampleMarkets();
+
+        mAdapter = new MarketAdapter(mMarkets);
+        mMarketList.setAdapter(mAdapter);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mMarketList.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int shortAnimation = getResources().getInteger(android.R.integer.config_shortAnimTime);
+                ViewUtils.crossfadeTwoViews(mMarketList, mSuggestion, shortAnimation);
+            }
+        }, 3000);
     }
 }
