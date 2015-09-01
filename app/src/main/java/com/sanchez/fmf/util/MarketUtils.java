575d1dc491ceb27b376eb23f49ff74be57c7674d
dakotasanchez <dakota@dakotasanchez.com>
2015-09-01 00:46:00 -0700

    Random market generator and view utils

--- app/src/main/java/com/sanchez/fmf/util/MarketUtils.java
+++ app/src/main/java/com/sanchez/fmf/util/MarketUtils.java
@@ -1,7 +1,25 @@
 package com.sanchez.fmf.util;
 
+import com.sanchez.fmf.model.MarketListItemModel;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by dakota on 8/31/15.
  */
 public class MarketUtils {
+
+    public static ArrayList<MarketListItemModel> getExampleMarkets() {
+        ArrayList<MarketListItemModel> mModels = new ArrayList<>();
+        Random r = new Random();
+        DecimalFormat df = new DecimalFormat("#.0");
+        for (int i = 0; i < 50; i++) {
+            double dist = r.nextDouble() * 50;
+            mModels.add(new MarketListItemModel(Integer.toString(i), "Definitely a Market\n" + i,
+                    Double.parseDouble(df.format(dist))));
+        }
+        return mModels;
+    }
 }
--- app/src/main/java/com/sanchez/fmf/util/ViewUtils.java
+++ app/src/main/java/com/sanchez/fmf/util/ViewUtils.java
@@ -1,7 +1,31 @@
 package com.sanchez.fmf.util;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.view.View;
+
 /**
  * Created by dakota on 8/31/15.
  */
 public class ViewUtils {
+
+    public static void crossfadeTwoViews(final View inView, final View outView, int duration) {
+        inView.setAlpha(0f);
+        inView.setVisibility(View.VISIBLE);
+
+        inView.animate()
+                .alpha(1f)
+                .setDuration(duration)
+                .setListener(null);
+
+        outView.animate()
+                .alpha(0f)
+                .setDuration(duration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        outView.setVisibility(View.GONE);
+                    }
+                });
+    }
 }
