de59e222ac32d92fb11d3ccbc0caa3a153a7f115
dakotasanchez <dakota@dakotasanchez.com>
2015-09-01 22:24:31 -0700

    Better random market generator

--- app/src/main/java/com/sanchez/fmf/util/MarketUtils.java
+++ app/src/main/java/com/sanchez/fmf/util/MarketUtils.java
@@ -13,11 +13,17 @@ public class MarketUtils {
 
     public static ArrayList<MarketListItemModel> getExampleMarkets() {
         ArrayList<MarketListItemModel> mModels = new ArrayList<>();
+        String alphabet = " 1234567890 abcdefghijklmnopqrstuvwxyz ";
         Random r = new Random();
         DecimalFormat df = new DecimalFormat("#.0");
         for (int i = 0; i < 50; i++) {
             double dist = r.nextDouble() * 50;
-            mModels.add(new MarketListItemModel(Integer.toString(i), "Definitely a Market\n" + i,
+            int marketLength = r.nextInt(50) + 1;
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < marketLength; j++) {
+                sb.append(alphabet.charAt(r.nextInt(alphabet.length())));
+            }
+            mModels.add(new MarketListItemModel(Integer.toString(i), sb.toString(),
                     Double.parseDouble(df.format(dist))));
         }
         return mModels;
