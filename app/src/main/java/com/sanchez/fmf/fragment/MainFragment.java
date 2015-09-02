58153275391180637e0112cd27d1cb2c5bb465c2
dakotasanchez <dakota@dakotasanchez.com>
2015-09-02 01:04:32 -0700

    Implemented Search place autocomplete v1

--- app/src/main/java/com/sanchez/fmf/fragment/MainFragment.java
+++ app/src/main/java/com/sanchez/fmf/fragment/MainFragment.java
@@ -1,15 +1,48 @@
 package com.sanchez.fmf.fragment;
 
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.sanchez.fmf.R;
+import com.sanchez.fmf.adapter.PlaceAutocompleteAdapter;
 
-public class MainFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener{
+
+    public String TAG = MainFragment.class.getSimpleName();
+
+    @Bind(R.id.search_autocomplete)
+    AutoCompleteTextView mSearchAutocomplete;
+
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    private static final LatLngBounds BOUNDS_NORTH_AMERICA = new LatLngBounds(new LatLng(18.000000,
+            -64.000000), new LatLng(67.000000, -165.000000));
+
+    private GoogleApiClient googleApiClient;
+    private PlaceAutocompleteAdapter mAutocompleteAdapter;
 
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
@@ -22,15 +55,68 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
+
+        googleApiClient = new GoogleApiClient
+                .Builder(getActivity())
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage((FragmentActivity) getActivity(), GOOGLE_API_CLIENT_ID, this)
+                .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, v);
+
+        mAutocompleteAdapter = new PlaceAutocompleteAdapter(getActivity(),
+                android.R.layout.simple_list_item_1, googleApiClient, BOUNDS_NORTH_AMERICA, null);
+        mSearchAutocomplete.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+        mSearchAutocomplete.setAdapter(mAutocompleteAdapter);
+
+        return v;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(getActivity(), "Google Places API unavailable right now. Error code:" +
+                connectionResult.getErrorCode(), Toast.LENGTH_LONG).show();
+    }
+
+    private class GetCoordinatesFromLocation extends AsyncTask<String, Void, ArrayList<Double>> {
+
+        @Override
+        protected ArrayList<Double> doInBackground(String... location) {
+            ArrayList<Double> returnList = new ArrayList<Double>();
+            try {
+                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                List<Address> addresses = geocoder.getFromLocationName(location[0], 1);
+                if(addresses.size() > 0) {
+                    Address address = addresses.get(0);
+                    returnList.add(address.getLatitude());
+                    returnList.add(address.getLongitude());
+                    return returnList;
+                } else {
+                    return null;
+                }
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+                Toast.makeText(getActivity(), "Geocoder error", Toast.LENGTH_LONG).show();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Double> result) {
+            if(result != null) {
+                Toast.makeText(getActivity(), "Coords: lat=" + result.get(0) + " lon=" + result.get(1),
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
