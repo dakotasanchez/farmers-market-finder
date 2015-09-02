a095fb81fde7b1fd6734897fc7060fd67a0a995d
dakotasanchez <dakota@dakotasanchez.com>
2015-09-01 23:39:49 -0700

    Added fragment for home screen, removed nav drawer, and removed unneeded assets

--- app/src/main/java/com/sanchez/fmf/MainActivity.java
+++ app/src/main/java/com/sanchez/fmf/MainActivity.java
@@ -1,35 +1,25 @@
 package com.sanchez.fmf;
 
-import android.content.res.Configuration;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.support.v4.widget.DrawerLayout;
+import android.view.Window;
+import android.view.WindowManager;
 
-import com.sanchez.fmf.fragment.MarketListFragment;
+import com.sanchez.fmf.fragment.MainFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.drawer_layout)
-    DrawerLayout mDrawerLayout;
     @Bind(R.id.toolbar_main_activity)
     Toolbar mToolbar;
-    @Bind(R.id.navigation_view)
-    NavigationView mNavView;
-
-    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,132 +29,41 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
 
-        final ActionBar ab = getSupportActionBar();
-        if(ab != null) {
-            ab.setDisplayHomeAsUpEnabled(true);
+        Window w = getWindow();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            w.setStatusBarColor(getResources().getColor(R.color.primary_dark));
         }
 
-        setupDrawerContent(mNavView);
+        FragmentManager fm = getFragmentManager();
+        Fragment mainFragment = fm.findFragmentById(R.id.container_main_activity);
 
-        mDrawerToggle = setupDrawerToggle();
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        // Default to first frag for now
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.container, MarketListFragment.newInstance()).commit();
-    }
-
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        return new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
-
-            @Override
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                super.onDrawerSlide(drawerView, 0); // disable arrow upon open completion
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, 0); // disable toggle icon animation
-            }
-        };
-    }
-
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-            new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem menuItem) {
-                    selectDrawerItem(menuItem);
-                    return true;
-                }
-            });
-    }
-
-    public void selectDrawerItem(MenuItem menuItem) {
-        Fragment fragment = null;
-
-        Class fragmentClass;
-        switch(menuItem.getItemId()) {
-            case R.id.nav_first_fragment:
-                fragmentClass = MarketListFragment.class;
-                break;
-            case R.id.nav_second_fragment:
-                fragmentClass = null;
-                break;
-            default:
-                fragmentClass = MarketListFragment.class;
+        if (mainFragment == null) {
+            mainFragment = MainFragment.newInstance();
+            fm.beginTransaction()
+                    .add(R.id.container_main_activity, mainFragment)
+                    .commit();
         }
 
-        if(fragmentClass != null) {
-            try {
-                fragment = (Fragment) fragmentClass.newInstance();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            // Insert the fragment by replacing any existing fragment
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
-
-            // Highlight the selected item, update the title, and close the drawer
-            menuItem.setChecked(true);
-            setTitle(menuItem.getTitle());
-        } else {
-            // launch fragment dialog saying map is coming soon
-        }
-        mDrawerLayout.closeDrawers();
+//        final ActionBar ab = getSupportActionBar();
+//        if(ab != null) {
+//            ab.setDisplayHomeAsUpEnabled(true);
+//        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-//            // Only show items in the action bar relevant to this screen
-//            // if the drawer is not showing. Otherwise, let the drawer
-//            // decide what to show in the action bar.
-//            getMenuInflater().inflate(R.menu.main, menu);
-//            restoreActionBar();
-//            return true;
-//        }
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-        if(mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
         switch (item.getItemId()) {
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
---
+++ app/src/main/java/com/sanchez/fmf/fragment/MainFragment.java
@@ -0,0 +1,36 @@
+package com.sanchez.fmf.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sanchez.fmf.R;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
