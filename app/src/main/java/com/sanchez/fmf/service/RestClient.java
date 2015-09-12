e49bb5b42fe9d7827cd1148d2643da595cec6d3a
dakotasanchez <dakota@dakotasanchez.com>
2015-09-12 00:00:10 -0700

    Use okhttp for http layer of API requests

--- app/src/main/java/com/sanchez/fmf/service/RestClient.java
+++ app/src/main/java/com/sanchez/fmf/service/RestClient.java
@@ -2,8 +2,10 @@ package com.sanchez.fmf.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.OkHttpClient;
 
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
 
 /**
@@ -19,12 +21,12 @@ public class RestClient {
 //        Gson gson = new GsonBuilder()
 //                .registerTypeAdapter(MarketListModel.class, new MarketListModelDeserializer())
 //                .create();
-        Gson gson = new GsonBuilder()
-                .create();
+        Gson gson = new Gson();
 
         RestAdapter retrofit = new RestAdapter.Builder()
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .setEndpoint(BASE_URL)
+                .setClient(new OkClient(new OkHttpClient()))
                 .setConverter(new GsonConverter(gson))
                 .build();
 
