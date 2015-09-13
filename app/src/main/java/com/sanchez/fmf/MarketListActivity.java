b761ebb962133f8cfefdd73d9e8921ab2794204b
dakotasanchez <dakota@dakotasanchez.com>
2015-09-12 22:40:32 -0700

    Correct "Up" button action

--- app/src/main/java/com/sanchez/fmf/MarketListActivity.java
+++ app/src/main/java/com/sanchez/fmf/MarketListActivity.java
@@ -52,22 +52,20 @@ public class MarketListActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_market_list, menu);
+        getMenuInflater().inflate(R.menu.menu_market_list, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
