2ae88efaabc0dc85e56b82666080727b19955d24
dakotasanchez <dakota@dakotasanchez.com>
2015-09-15 23:14:11 -0700

    Remove undesirable toast

--- app/src/main/java/com/sanchez/fmf/adapter/PlaceAutocompleteAdapter.java
+++ app/src/main/java/com/sanchez/fmf/adapter/PlaceAutocompleteAdapter.java
@@ -14,10 +14,8 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
@@ -102,8 +100,6 @@ public class PlaceAutocompleteAdapter
             // Confirm that the query completed successfully, otherwise return null
             final Status status = autocompletePredictions.getStatus();
             if (!status.isSuccess()) {
-                Toast.makeText(getContext(), "Error contacting API: " + status.toString(),
-                        Toast.LENGTH_SHORT).show();
                 Log.e(TAG, "Error getting autocomplete prediction API call: " + status.toString());
                 autocompletePredictions.release();
                 return null;
